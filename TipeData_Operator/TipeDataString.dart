413c41f821ce9543c74b22e1cf5b24581596efa0
raelhan <hannn9491@gmail.com>
2023-12-13 09:29:34 +0700

    add Tipe Data String

--- TipeData_Operator/TipeDataString.dart
+++ TipeData_Operator/TipeDataString.dart
@@ -1,3 +1,20 @@
 void main() {
-  
+  String nama = 'Raihan Zhaky';
+  String daftarKucing = 'Persia, Anggora, Sphinx';
+  var nomor = 95;
+
+  // cek apakah mengandung string tertentu
+  print(nama.contains('raihan'));
+
+  // mengubah jadi huruf kecil
+  print(nama.toLowerCase());
+
+  // mengubah jadi huruf besar
+  print(nama.toUpperCase());
+
+  // mengubah menjadi string
+  print(nomor.toString());
+
+  // mengubah menjadi list
+  print(daftarKucing.split('*'));
 }
