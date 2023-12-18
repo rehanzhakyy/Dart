48ff62ee825195676fa2d289b9336af393b19263
raelhan <hannn9491@gmail.com>
2023-12-18 10:48:12 +0700

    add If & Else

---
+++ Method_ControlFlow/If_Else.dart
@@ -0,0 +1,15 @@
+void main() {
+  int angka = 5;
+  if (angka % 2 == 0) {
+    print('Genap');
+  } else {
+    print('Ganjil');
+  }
+
+  String nama = 'Eka';
+  if (nama == 'Raihan') {
+    print('Ini Raihan');
+  } else {
+    print('Ini bukan Raihan!');
+  }
+}
