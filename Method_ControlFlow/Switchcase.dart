6cb39d51fcc389766775f1b5938d05155ad21f60
raelhan <hannn9491@gmail.com>
2023-12-18 15:45:57 +0700

    add Switch Case

---
+++ Method_ControlFlow/Switchcase.dart
@@ -0,0 +1,15 @@
+void main() {
+  var nilai = 'C';
+  switch (nilai) {
+    case 'A':
+      print('Sangat Baik');
+      break;
+    case 'B':
+      print('Baik');
+    case 'C':
+      print('Cukup');
+    default:
+      print('Tidak Valid');
+      break;
+  }
+}
