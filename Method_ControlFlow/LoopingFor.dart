aefd64cee0326452b816800c94b9ae51fdb65bb6
raelhan <hannn9491@gmail.com>
2023-12-19 10:16:38 +0700

    add Loopin For

---
+++ Method_ControlFlow/LoopingFor.dart
@@ -0,0 +1,16 @@
+void main() {
+  print('Menggunakan For');
+  int n = 100;
+  for (int i = 1; i <= n; i++) {
+    if (i % 2 == 1) {
+      print(i);
+    }
+  }
+
+  List daftarMakanan = ['Sate', 'Nasi Goreng', 'Bakso'];
+  print('Daftar Makanan');
+  print(daftarMakanan);
+  for (int i = 0; i < daftarMakanan.length; i++) {
+    print(daftarMakanan[i]);
+  }
+}
