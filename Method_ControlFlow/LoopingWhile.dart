4aa0aca1975473f5efd029d66080e7d2b1c76844
raelhan <hannn9491@gmail.com>
2023-12-19 10:20:00 +0700

    add Looping While

---
+++ Method_ControlFlow/LoopingWhile.dart
@@ -0,0 +1,17 @@
+void main() {
+  int i = 1;
+  while (i <= 100) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+    i++;
+  }
+
+  List daftarMakanan = ['Sate', 'Nasi Goreng', 'Bakso'];
+  int index = 0;
+  print('Daftar Makanan');
+  while (index < daftarMakanan.length) {
+    print(daftarMakanan[index]);
+    index++;
+  }
+}
