43526642dc526eda09a3cb15d18fc0efdfe6d772
rehanzhakyy <hannn9491@gmail.com>
2024-06-22 03:16:41 +0700

    add Variables

---
+++ 00-Introduction/02-Variables.dart
@@ -0,0 +1,21 @@
+void main() {
+  // String
+  var name = "Raihan Zhaky";
+  // Integer
+  var year = 2001;
+  // Float
+  var laptopDiameter = 5.9;
+  // List
+  var mentors = ['Raihan', 'Reynaldi', 'Rahmad', 'Reza', 'Nabila'];
+  // Image
+  var image = {
+    'tags': ['Raihan'],
+    'url': 'https://github.com/raihanzhaky'
+  };
+
+  print(name);
+  print(year);
+  print(laptopDiameter);
+  print(mentors);
+  print(image);
+}
